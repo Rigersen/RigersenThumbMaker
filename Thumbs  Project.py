04101a081f03b1deae7680680663466853042150
Rigersen <rigersen@gmail.com>
2024-06-16 09:56:02 -0300

    finale

--- Thumbs  Project.py
+++ Thumbs  Project.py
@@ -25,7 +25,7 @@ def adicionar_texto_na_imagem(imagem_caminho, texto, numero, fonte_caminho, tama
     altura_numero = bbox_numero[3] - bbox_numero[1]
     
     # calcular a posição centralizada para o texto
-    posicao_texto = ((largura_imagem - largura_texto) / 2, (altura_imagem - (altura_texto + altura_numero)) / 2)
+    posicao_texto = ((largura_imagem - largura_texto) / 2, (altura_imagem-100 - (altura_texto + altura_numero)) / 2)
     posicao_numero = ((largura_imagem - largura_numero) / 2, posicao_texto[1] + altura_texto)
     
     # desenhar a sombra do texto
@@ -37,7 +37,7 @@ def adicionar_texto_na_imagem(imagem_caminho, texto, numero, fonte_caminho, tama
     desenho_sombra.text(posicao_sombra_numero, numero, font=fonte_numero, fill=cor_sombra)
     
     # aplicar desfoque à sombra
-    sombra = sombra.filter(ImageFilter.GaussianBlur(radius=5))
+    sombra = sombra.filter(ImageFilter.GaussianBlur(radius=6))
     
     # compor a sombra desfocada na imagem original
     imagem = Image.alpha_composite(imagem, sombra)
@@ -59,15 +59,15 @@ def adicionar_texto_na_imagem(imagem_caminho, texto, numero, fonte_caminho, tama
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 
 # configurações
-imagem_caminho = os.path.join(diretorio_atual, 'nome_da_img.png')  # Caminho da imagem a ser carregada
-fonte_caminho = os.path.join(diretorio_atual, 'comic_sans.ttf')  # Caminho da fonte TrueType (.ttf)
-tamanho_fonte_texto = 144  # Tamanho da fonte para o texto principal
-tamanho_fonte_numero = 144  # Tamanho da fonte para o número
-cor_texto = (255, 255, 255)  # Cor do texto (em RGB)
-cor_sombra = (0, 0, 0, 255)  # Cor da sombra (em RGBA)
+imagem_caminho = os.path.join(diretorio_atual, 'thumb.jpg')  # diretorio da imagem
+fonte_caminho = os.path.join(diretorio_atual, 'comic_sans.ttf')  # diretorio da fonte (.ttf)
+tamanho_fonte_texto = 144  # Tamanho da fonte do titulo
+tamanho_fonte_numero = 144  # tamanho do número
+cor_texto = (255, 255, 255)  # cor do texto (RGB)
+cor_sombra = (0, 0, 0, 255)  # cor da sombra (RGBA)
 
 for i in range(1, 3):
-    texto = 'Série'
+    texto = 'Titulo'
     numero = f'#{i}'
     salvar_como = os.path.join(diretorio_atual, f'Thumb{i}.png')
     adicionar_texto_na_imagem(imagem_caminho, texto, numero, fonte_caminho, tamanho_fonte_texto, tamanho_fonte_numero, cor_texto, cor_sombra, salvar_como)
